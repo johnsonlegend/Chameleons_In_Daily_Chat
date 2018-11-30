74de3cca28e860e94050529cd2e06cdb934f7e6a
wangych <wangych@umich.edu>
2018-11-29 19:26:21 -0500

    Just proved that this effect is false

--- convergence.py
+++ convergence.py
@@ -1,9 +1,21 @@
 import pickle
 import sys
 import re
+import numpy as np
 
-#token: list of tags
+#liwc: [token : list of tags]
 liwc = {}
+fw_type = {
+	'PRONOUN' : 0,
+	'IPRON' : 1,
+	'ARTICLE' : 2,
+	'PREP' : 3,
+	'AUXVERB' : 4,
+	'ADVERB' : 5,
+	'CONJ' : 6,
+	'NEGATE' : 7,
+	'QUANT' : 8
+}
 
 def load_liwc():
 	f = open("LIWC.2015.all","r")
@@ -18,12 +30,17 @@ def load_liwc():
 		line = f.readline()
 	f.close()
 
-def load_conversation():
-	f = open("TRN_output/SBC001.pkl","rb")
+def load_conversation(num):
+	if num < 10:
+		file = "TRN_output/SBC00" + str(num) + ".pkl"
+	else:
+		file = "TRN_output/SBC0" + str(num) + ".pkl"
+	f = open(file,"rb")
 	conv = pickle.load(f)
 	f.close()
 	return conv
 
+# combine adjacent lines by the same person
 def combine_conv(conv):
 	conv_combined = []
 	line = conv[0]
@@ -36,11 +53,46 @@ def combine_conv(conv):
 	conv_combined.append(line)
 	return conv_combined
 
-if __name__ == '__main__':
-	load_liwc()
-	conv = load_conversation()
-	conv_combined = combine_conv(conv)
+# extract function words
+def extract_fw(line):
 	
 
+	fw = np.zeros(9)
+	for w in line[1]:
+		if liwc.get(w):
+			for type in liwc[w]:
+				if type in fw_type:
+					fw[fw_type[type]] = 1
 
+	return line[0], fw
+
+def analysis(conv):
+	conv_combined = combine_conv(conv)
+
+	fw_avg = np.zeros(9)
+	fw_all = []
+	for line in conv_combined:
+		person, fw = extract_fw(line)
+		fw_all.append(fw)
+		fw_avg += fw
+
+	fw_given = np.zeros(9)
+
+	for i in range(len(fw_all) - 1):
+		for tag_idx in range(9):
+			if fw_all[i][tag_idx] == 1:
+				fw_given[tag_idx] += fw_all[i + 1][tag_idx]
+	fw_avg = sum(fw_all)/len(fw_all)
+
+	fw_given_avg = fw_given / (sum(fw_all) - fw_all[-1])
+	# print(fw_given_avg - fw_avg)
+
+	return fw_given_avg - fw_avg
+if __name__ == '__main__':
+	load_liwc()
+	res = np.zeros(9)
+	for num in range (1,61):
+		conv = load_conversation(num)
+		res += analysis(conv)
+	print(res/60)
 	
